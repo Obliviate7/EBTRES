45e9ccbad3a8b260e2b3859e918ef890eeed6b73
Unknown <catalina-delgado@outlook.com>
2017-12-05 20:39:32 -0300

    ahi va

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Route::get('/', 'HomeController@index');
 // - Auth
 // Authentication Routes...
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
@@ -30,7 +31,6 @@ Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm'
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
-
 
 Auth::routes();
 
